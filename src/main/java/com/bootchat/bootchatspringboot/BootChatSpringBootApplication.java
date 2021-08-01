a757b8dfddf1c0dbb8eb0d94352e490a3a542bf1
iamducthanh <ducthanh260801@gmail.com>
2021-08-01 17:34:13 +0700

    deploy

---
+++ src/main/java/com/bootchat/bootchatspringboot/BootChatSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bootchat.bootchatspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootChatSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootChatSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootchat/bootchatspringboot/BootChatSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootchat.bootchatspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootChatSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
