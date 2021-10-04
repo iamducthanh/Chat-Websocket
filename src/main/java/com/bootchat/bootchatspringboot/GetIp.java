1292216cc7c43a9d21e3b407ea037fbce87f9844
iamducthanh <65590543+iamducthanh@users.noreply.github.com>
2021-10-04 16:51:42 +0700

    Update GetIp.java

--- src/main/java/com/bootchat/bootchatspringboot/GetIp.java
+++ src/main/java/com/bootchat/bootchatspringboot/GetIp.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.Inet4Address;
