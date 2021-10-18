9dab4f07ea519e04b7259f149a355598e2a87694
iamducthanh <65590543+iamducthanh@users.noreply.github.com>
2021-10-18 17:29:24 +0700

    Update BootChatSpringBootApplication.java

--- src/main/java/com/bootchat/bootchatspringboot/BootChatSpringBootApplication.java
+++ src/main/java/com/bootchat/bootchatspringboot/BootChatSpringBootApplication.java
@@ -2,25 +2,12 @@ package com.bootchat.bootchatspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BootChatSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootChatSpringBootApplication.class, args);
-    }
-    
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-            }
-        };
     }
 
 }
